9eb3e86cbf66af04949c4dfb3c1a5e3bb94ae54f
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 02:46:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   
   def execute_transaction #sender=amanda expect(sender.balance).to eq(950)
     if @sender.status != "open" || @receiver.status != "open"
-      if sender.balance >= amount #both accounts must be open, the sender balance is >= amount being sent
+      if @sender.balance >= @amount #both accounts must be open, the sender balance is >= amount being sent
       @status = "rejected"
       "Transaction rejected. Please check your account balance." #spec file 
     elsif @status == "complete" #spec file "complete" 
