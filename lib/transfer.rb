b7906e3f3014693956c994b77f6348f756f1630a
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 02:10:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,13 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver 
+  
+  def initialize(sender, receiver, number)
+    @sender = sender 
+    @receiver = receiver
+    @status = "pending" 
+  end   
+  
+  
+  
 end
